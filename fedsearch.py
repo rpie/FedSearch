6c6c866c40343dda0a56a81f421f1b3f3e9294d0
HellSec <57877984+rpie@users.noreply.github.com>
2021-02-24 19:00:57 -0800

    Update fedsearch.py

--- fedsearch.py
+++ fedsearch.py
@@ -7,7 +7,7 @@ class api:
         '''
 
     def search(self, key, query):
-        r = requests.post('https://fedsearch.cf/API/search_api.php', data={'search': query, 'submit': '', 'key': key,}).text
+        r = requests.post('https://fedsearch.cf/API/search_api.php', data={'search': query, 'submit': '', 'key': key,}).json()
         if 'DOCTYPE' in r:
             error = f'\033[31m[Error]\033[0m Cloudflare Ratelimit'
             return(error)
@@ -47,3 +47,31 @@ class api:
             return(error)
         else:
             return(r)
+
+    def prettyprint(self, key, query):
+        r = requests.post('https://fedsearch.cf/API/search_api.php', data={'search': query, 'submit': '', 'key': key,}).json()
+        if 'DOCTYPE' in r:
+            error = f'\033[31m[Error]\033[0m Cloudflare Ratelimit'
+            return(error)
+        elif 'Blocked' in r:
+            error = f'\033[31m[Error]\033[0m Blocked By Cloudflare'
+            return(error)
+        elif 'Dirrect' in r:
+            error = f'\033[31m[Error]\033[0m Dirrect Access Error'
+            return(error)
+        else:
+            results = []
+            for result in r:
+                email, password, ip, token, database = result['email'], result['password'], result['ip'], result['token'], result['database']
+                results.append(f'''
+                   \033[31m______
+\033[31m==================[\033[0mResult\033[31m]==================\033[0m
+
+Email       :   {email}        
+Password    :   {password}
+Token       :   {token}
+IP Address  :   {ip}
+Database    :   {database}
+
+\033[31m============================================\033[0m''')
+            return(results)
